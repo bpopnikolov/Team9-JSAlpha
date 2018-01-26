81c4d91d0100cd8194cbd60d023ee78ce48239a9
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-01-26 16:26:33 +0200

    added git repo anchor, nav buttons event listeners

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ import * as headerController from './controllers/core/header.js';
 
 
 $('header').load('./views/core/header.html', headerController.init);
+$('.app-container').load('../../views/core/home.html'); 
 
--- controllers/core/header.js
+++ controllers/core/header.js
@@ -1,5 +1,33 @@
 export function init() {
 
-}
+    var activeRoute = window.location.hash;
 
- 
+    $(window).on('hashchange', function(e) {
+        var currentRoute = e.target.location.hash;
+        if (currentRoute !== activeRoute) {
+            $('.list .active').removeClass('active');
+            activeRoute = currentRoute;
+            var buttonId = currentRoute.substring(1) + '-btn';
+            $(`#${buttonId}`).addClass('active');
+        }
+    });
+
+    $('#git-btn').on('click', function() {
+        window.open('https://github.com/bpopnikolov/Team9-JSAlpha', '_blank');
+    });
+
+    $('.git-logo').attr('src', '../../assets/images/GitHub-Mark-32px.png');
+
+    $('.logo').on('click', function() {
+        $('.app-container').load('../../views/core/home.html');
+    });
+
+    $('#home-btn').on('click', function() {
+        $('.app-container').load('../../views/core/home.html');
+    });
+
+    $('#recipes-btn').on('click', function() {
+        $('.app-container').load('../../views/recipes/recipes.html');
+    });
+
+}
