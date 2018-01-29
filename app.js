e2c97d18d4707cc0ddefc138ef7e0aac80723b80
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-01-29 14:07:39 +0200

    refactoring dbservice and app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import * as HeaderController from './controllers/core/header.js';
-
-$('header').load('./views/core/header.html', HeaderController.init);
-
+import * as DBService from './services/db-service/database-service.js';
 
 
-
+$(function() {
+    $('header').load('./views/core/header.html', HeaderController.init);
+});
