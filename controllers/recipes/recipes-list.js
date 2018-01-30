3cd49941bc100084185001801a392b7c10f0c2e8
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-01-30 14:10:51 +0200

    added recipes.css and recipes-list.css, added logic for loading all recipes from db

--- controllers/recipes/recipes-list.js
+++ controllers/recipes/recipes-list.js
@@ -3,24 +3,35 @@ import * as RecipesService from './recipes-service.js';
 export function init() {
 
     RecipesService.getRecipes().then(function (data) {
-        var $list = $('#recipesList');
+        var $container = $('.recipes-list-container');
         var result = [];
         for (const key in data) {
-            if (data.hasOwnProperty(key)) {
+            if (typeof data.key === 'undefined') {
                 result.push(data[key]);
             }
         }
 
         result.forEach(recipe => {
-            var $listEl = $('<li>');
-            $listEl.text(recipe.name);
-            $list.append($listEl);
-        });
+            var $wrapper = $('<div>');
+            $wrapper.addClass('');
+            var $title = $('<p>');
+            $title.addClass('');
+            var $imgUrl = $('<img>');
+            $imgUrl.addClass('');
+
+            $title.text(recipe.name);
+            $imgUrl.attr('src', recipe.imgUrl);
+            $wrapper.append($title);
+            $wrapper.append($imgUrl);
+            $container.append($wrapper);
+        }); 
     });
 
     $('#add-recipe').on('click', function() {
         RecipesService.saveRecipe({
-            name: 'Test Recipe',
+            name: 'Yogurt and blueberry popsicles',
+            description: `Good Living/Sweets/Desserts`,
+            ingredients: ['yogurt', 'blueberries']
         });
     });
 }
