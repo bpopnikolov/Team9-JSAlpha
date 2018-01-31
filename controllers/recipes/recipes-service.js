680023bb844145ec65863d6cbce64b7dbb373b13
Borislav Popnikolov <borislav.popnikolov@gmail.com>
2018-01-31 10:32:26 +0200

    added delete method in DbService, added sidenav, refacotring in recipesService

--- controllers/recipes/recipes-service.js
+++ controllers/recipes/recipes-service.js
@@ -1,24 +1,37 @@
 import * as DbService from '../../services/db-service/database-service.js';
 
 export function saveRecipe(recipe) {
-    DbService.writeData('recipes', recipe, function(err) {
-        if (err) {
-            console.log(err);
-        }
-    })
-}
-
-export function getRecipes() {
-    return new Promise(function (resolve, reject) {
-        DbService.getData('recipes/', function (recipes, err) {
+    return new Promise(function(resolve, reject) {
+        DbService.writeData('recipes', recipe, function(err) {
             if (err) {
                 reject(err);
             } else {
-                resolve(recipes);
+                resolve(`The recipes was saved successfully!`);
             }
-
         });
-
     });
+}
 
+export function getRecipes() {
+    return new Promise(function(resolve, reject) {
+        DbService.getData('recipes/', function(data, err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+export function deleteRecipe(id) {
+    return new Promise(function(resolve, reject) {
+        DbService.deleteData('recipes/', id, function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(`The recipe was deleted successfully!`);
+            }
+        });
+    });
 }
