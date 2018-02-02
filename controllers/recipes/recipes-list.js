c50ab4782bc55c4454d1021ace6cf3a27e7aca3c
Victoria Tsvetanova <victoria>
2018-02-02 10:28:19 +0200

    added flex foodbox

--- controllers/recipes/recipes-list.js
+++ controllers/recipes/recipes-list.js
@@ -2,7 +2,7 @@ import * as RecipesService from './recipes-service.js';
 
 export function init() {
 
-    RecipesService.getRecipes().then(function(data) {
+    RecipesService.getRecipes().then(function (data) {
         var $container = $('.recipes-list-container');
         var result = [];
         for (const key in data) {
@@ -16,29 +16,52 @@ export function init() {
             $wrapper.addClass('foodBox');
             var $title = $('<h3>');
             $title.addClass('nameOfRecipe');
+            // ------------------------------------ 
+            //var $titleFrame = $('<div>');
+            //$titleFrame.append($title);
+            //$titleFrame.addClass('texts');
+            // ------------------------------------
             var $desc = $('<p>');
+            $desc.addClass('nameOfRecipe');
+            // ------------------------------------
+            var $descFrame = $('<div>');
+            $descFrame.append($title);
+            $descFrame.append($desc);
+            $descFrame.addClass('texts');
+            // ------------------------------------
             var $imgUrl = $('<img>');
             $imgUrl.addClass('recipeImage');
+            // ------------------------------------ 
+            var $imgFrame = $('<div>');
+            $imgFrame.append($imgUrl);
+            $imgFrame.addClass('image');
+            // ------------------------------------
 
             $title.text(recipe.name);
             $desc.text(recipe.description);
             $imgUrl.attr('src', recipe.imgUrl);
-            $wrapper.append($title);
-            $wrapper.append($desc);
-            $wrapper.append($imgUrl);
+            //$wrapper.append($title);
+            //$wrapper.append($desc);
+            //$wrapper.append($imgUrl); 
+            // ------------------------------------
+            //$wrapper.append($titleFrame);
+            $wrapper.append($descFrame);
+            $wrapper.append($imgFrame);
+            // ------------------------------------
+
             $container.append($wrapper);
         });
     });
 
-    $('#add-recipe').on('click', function() {
+    $('#add-recipe').on('click', function () {
         RecipesService.saveRecipe({
             name: 'Yogurt and blueberry popsicles',
             description: `Good Living/Sweets/Desserts`,
             ingredients: ['yogurt', 'blueberries'],
             imgUrl: `https://www.daringgourmet.com/wp-content/uploads/2015/01/Greek-Yogurt-2-web-728px.jpg`
-        }).then(function(msg) {
+        }).then(function (msg) {
             console.log(msg);
-        }).catch(function(err) {
+        }).catch(function (err) {
             console.log(err);
         });
     });
