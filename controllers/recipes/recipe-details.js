e1d98135f9dd88f4ebaf2e28f8a795288bce51eb
Plamen Ivanov <plamgit@mail.bg>
2018-02-02 15:41:01 +0200

    add-recipe.css styling the forms

--- controllers/recipes/recipe-details.js
+++ controllers/recipes/recipe-details.js
@@ -1,3 +1,17 @@
 export function init() {
+    $("#edit-recipe").on('click',function(){
+        $('#modalEdit').show();
+    });
 
+    $('#close-edit').on('click',function(){
+        $('#modalEdit').hide();
+        
+    })
+
+    $('#remove-recipe').on('click',function(){
+        var result = confirm("Want to delete?");
+        if (result) {
+            alert('this recipe is deleted');
+        }
+    })
 }
